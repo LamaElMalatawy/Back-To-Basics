1bffd5be781942add8c66c989ef0547582d710e6
LamaElMalatawy <lamaelmalatawy@gmail.com>
2024-10-04 23:33:20 +0300

    JDBC

---
+++ JDBC/JDBC/src/Main.java
@@ -0,0 +1,15 @@
+import database.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Database database = new Database();
+        database.getBookById(1);
+
+    }
+}
---
+++ JDBC/JDBC/src/entities/Book.java
@@ -0,0 +1,51 @@
+package entities;
+
+
+public class Book {
+    private int id;
+    private String name;
+    private String summary;
+    private String rating;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", summary='" + summary + '\'' +
+                ", rating='" + rating + '\'' +
+                '}';
+    }
+}
